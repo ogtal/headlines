6f9045ea07c173c45d2acb22c529cdfacf6d244c
edin-ogtal <edin@ogtal.dk>
2019-03-24 20:27:12 +0100

    refactor

--- update_ignored_links.py
+++ update_ignored_links.py
@@ -1,8 +1,47 @@
 import pandas as pd
 
+from db_raw import Headlines_Raw
 from orm import Whitelist_Media, Media, Ignored_Links
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from ogtal_secrets import mysql_stat_url
+
+
+def find_ignorable_links(df):
+    link_id_count = df.groupby('link_id').nunique() 
+    stable_link_ids = link_id_count[link_id_count.to_link_url == 1].index.to_list()
+    df_stable_link_ids = df[df.link_id.isin(stable_link_ids)]
+
+    links_to_ignore = list(df_stable_link_ids.to_link_url.unique())
+    return(links_to_ignore)
+
+def get_raw_data(media_name):
+    result = session.\
+                query(Whitelist_Media).\
+                filter(Whitelist_Media.name==name).first()
+
+    url_list = []
+    for i in result.urls:
+        url_list.append(i.url)
+
+    q_stmt = session_raw.\
+                query(Headlines_Raw).\
+                filter(Headlines_Raw.frontpage_url.in_(url_list)).\
+                limit(100000).statement
+
+    df = pd.read_sql(q_stmt, engine_raw)
+    return(df)
+
+def add_links_to_db(links_to_ignore):
+    for link in links_to_ignore:
+        if not session.query(Ignored_Links).filter(Ignored_Links.url == link).count():
+            new_ig_link = Ignored_Links(url = link, media_id=media_id)
+            session.add(new_ig_link)
+            session.commit()
+     
+
+engine_raw = create_engine(mysql_stat_url.format(db="headlines"), pool_recycle=7200)
+session_raw = sessionmaker(bind=engine_raw)()
 
 engine = create_engine('sqlite:///test.db')
 session = sessionmaker(bind=engine)()
@@ -10,45 +49,10 @@ session = sessionmaker(bind=engine)()
 q_stmt = session.query(Whitelist_Media).statement
 media = pd.read_sql(q_stmt, session.bind) 
 
-df = pd.read_csv('headlines_raw.csv', index_col=0, sep=';',
-                   names=['id', 'date', 'frontpage_url', 'link_id', 
-                   'link_percent_max_id', 'to_link_url', 'link_text'],
-                   parse_dates=True, nrows=40000)
+for name in media.name[0:1]:
+    media_id = media[media.name == name].id.item()
 
+    df = get_raw_data(media_name)
+    links_to_ignore = find_ignorable_links(df)
+    add_links_to_db(links_to_ignore)
 
-#def add_new_media():
-#    for row in session.query(Whitelist_Media).all():
-#        for url in row.urls:
-
-
-
-for row in session.query(Whitelist_Media).all():
-    for url in row.urls:
-        sub_df = df[df.frontpage_url == url.url]
-        link_id_count = sub_df.groupby('link_id').nunique() 
-        stable_link_ids = link_id_count[link_id_count.to_link_url == 1].index.to_list()
-        df_stable_link_ids = sub_df[sub_df.link_id.isin(stable_link_ids)]
-
-        links_to_ignore = df_stable_link_ids.to_link_url.unique() 
-
-        #for link in links_to_ignore:
-           
- 
-#for media_name in media.name:
-#    link_list = 
-# Load data for the last year 
-
-#Loop over media whitelist
-
-    # Find links that don't change position over a year
-
-    # Add to ignored links
-
-    # Check if there are multilpe links - determine which to use and which to ignore. 
-
-#inspect and see if 
-
-# Find links that don't change over a month. 
-
-
-# Print a list of media we did'nt include for manual reference. 
